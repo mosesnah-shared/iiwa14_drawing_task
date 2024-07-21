b5e96d0166d6295eed3eeec3fa18d19a404cbf73
Moses C. Nah <mosesnah@mit.edu>
2024-07-20 23:17:27 -0400

    update

---
+++ MATLABscripts/checkFK.m
@@ -0,0 +1,15 @@
+%% Cleaning up + Environment Setup
+clear; close all; clc;
+
+% Set figure size and attach robot to simulation
+robot = iiwa14( 'high' );
+robot.init( );
+
+% Create animation
+anim = Animation( 'Dimension', 3, 'xLim', [-0.7,0.7], 'yLim', [-0.7,0.7], 'zLim', [0,1.4] );
+anim.init( );
+anim.attachRobot( robot )  
+
+% Update kinematics
+robot.updateKinematics( robot.q_init );
+anim.update( 0 );
